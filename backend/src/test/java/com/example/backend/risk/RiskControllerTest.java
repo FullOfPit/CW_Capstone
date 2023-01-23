77ba3ab9b59796ee9abba8272bb7cbfefe4930c0
Christian Weck <weck.chr@gmail.com>
2023-01-23 20:05:36 +0100

    Integration test for POST endpoint

--- backend/src/test/java/com/example/backend/risk/RiskControllerTest.java
+++ backend/src/test/java/com/example/backend/risk/RiskControllerTest.java
@@ -3,6 +3,7 @@ package com.example.backend.risk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,13 +27,41 @@ class RiskControllerTest {
         mvc.perform(get("/api/risk"))
                 .andExpect(status().isUnauthorized());
     }
-    
+
     @Test
     @WithMockUser
     void getAll_returnsEmptyWhenNoRisksRegistered () throws Exception {
         mvc.perform(get("/api/risk"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]", true));
+    }
+
+    @Test
+    @WithMockUser
+    void create_returnsRiskCorrectly () throws Exception {
+        //Given
+
+        String request = """
+                {
+                        "projectId": "Test Project",
+                        "riskName": "Test Risk",
+                        "riskDescription": "Test description",
+                        "riskReductionMeasures": ["measure 1" ,"measure 2"],
+                        "healthHazard": 1,
+                        "probability": 1,
+                        "frequency": 1
+                    }
+               
+                """;
+
+        //When - Then
+
+        mvc.perform(post("/api/risk")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request))
+                .andExpect(status().isOk())
+                .andExpect(content().json(request));
+
     }
 
 
