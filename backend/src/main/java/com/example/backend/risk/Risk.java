9c29d641a8f319c22a119a866fba2d628cb88e62
Christian Weck <weck.chr@gmail.com>
2023-01-23 18:25:08 +0100

    Set up main frontend pages; Set up risk data class

---
+++ backend/src/main/java/com/example/backend/risk/Risk.java
@@ -0,0 +1,25 @@
+package com.example.backend.risk;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Risk {
+    @Id
+    private String id;
+    private String projectId;
+    private String riskDescription;
+    private List<String> riskReductionMeasures;
+
+    private int healthHazard;
+    private int probability;
+    private int frequency;
+
+
+}
