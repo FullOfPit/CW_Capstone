59a1afb49ef5802965f7cadfaa7a82b603b7a026
Christian Weck <weck.chr@gmail.com>
2023-01-24 14:19:01 +0100

    Implemented Integration test for getAll

--- backend/src/test/java/com/example/backend/risk/RiskControllerTest.java
+++ backend/src/test/java/com/example/backend/risk/RiskControllerTest.java
@@ -14,6 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext
@@ -21,6 +23,24 @@ class RiskControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private RiskRepository riskRepository;
+
+
+    //                      -- Auxiliary
+    private Risk generateTestRisk () {
+        return new Risk(
+                "Test ID",
+                "Test Project ID",
+                "Test Risk",
+                "Test Description",
+                List.of("Test Measure 1", "Test Measure 2"),
+                1,
+                1,
+                1
+        );
+    }
 
     @Test
     void getAll_returns401WhenNotLoggedIn () throws Exception {
@@ -38,15 +58,41 @@ class RiskControllerTest {
 
     @Test
     @WithMockUser
+    void getAll_returnsListWhenFilled () throws Exception {
+
+        this.riskRepository.save(generateTestRisk());
+
+        String expected = """
+                [{
+                        "projectId": "Test Project ID",
+                        "riskName": "Test Risk",
+                        "riskDescription": "Test Description",
+                        "riskReductionMeasures": ["Test Measure 1", "Test Measure 2"],
+                        "healthHazard": 1,
+                        "probability": 1,
+                        "frequency": 1
+                    }]
+                
+                """;
+
+        //When - Then
+        mvc.perform(get("/api/risk"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expected));
+
+    }
+
+    @Test
+    @WithMockUser
     void create_returnsRiskCorrectly () throws Exception {
         //Given
 
         String request = """
                 {
-                        "projectId": "Test Project",
+                        "projectId": "Test Project ID",
                         "riskName": "Test Risk",
-                        "riskDescription": "Test description",
-                        "riskReductionMeasures": ["measure 1" ,"measure 2"],
+                        "riskDescription": "Test Description",
+                        "riskReductionMeasures": ["Test Measure 1", "Test Measure 2"],
                         "healthHazard": 1,
                         "probability": 1,
                         "frequency": 1
@@ -57,11 +103,11 @@ class RiskControllerTest {
         //When - Then
 
         mvc.perform(post("/api/risk")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
                 .andExpect(status().isOk())
                 .andExpect(content().json(request));
 
     }
-    
+
 }
