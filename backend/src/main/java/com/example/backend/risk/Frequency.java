6c3faec54cf49ff61dbec6bc7a996176dc795692
Christian Weck <weck.chr@gmail.com>
2023-01-26 12:51:34 +0100

    Refactored ENUMs (healthHazard, probability, frequency) to ints

--- backend/src/main/java/com/example/backend/risk/Frequency.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.risk;
-
-public enum Frequency {
-    ONCE,
-    RARELY,
-    OCCASIONALLY,
-    REGULARLY
-}
--- backend/src/main/java/com/example/backend/risk/Probability.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.risk;
-
-public enum Probability {
-    UNLIKELY,
-    POSSIBLE,
-    LIKELY,
-    HIGHLYLIKELY
-}
