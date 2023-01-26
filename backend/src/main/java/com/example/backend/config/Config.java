555ba9030949e5d6b8db0448dc90bc839ce5f3f5
Christian Weck <weck.chr@gmail.com>
2023-01-26 09:37:40 +0100

    refactor to exclude config dir from test coverage

--- backend/src/main/java/com/example/backend/config/Config.java
+++ backend/src/main/java/com/example/backend/config/Config.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.example.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
