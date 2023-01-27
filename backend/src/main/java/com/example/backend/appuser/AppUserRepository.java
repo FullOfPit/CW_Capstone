867855f10bf3d14dad10c414ffa088a5fa711778
Christian Weck <weck.chr@gmail.com>
2023-01-27 17:22:53 +0100

    refactor of appUser -> appuser

--- backend/src/main/java/com/example/backend/appuser/AppUserRepository.java
+++ backend/src/main/java/com/example/backend/appuser/AppUserRepository.java
@@ -1,4 +1,4 @@
-package com.example.backend.appUser;
+package com.example.backend.appuser;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
