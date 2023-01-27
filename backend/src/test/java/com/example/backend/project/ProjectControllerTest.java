3b9ef5c5d35c8d2164d1241c9936a83a842b07fa
Christian Weck <weck.chr@gmail.com>
2023-01-27 17:37:32 +0100

    integration test for GET endpoint for project controller

--- backend/src/test/java/com/example/backend/project/ProjectControllerTest.java
+++ backend/src/test/java/com/example/backend/project/ProjectControllerTest.java
@@ -1,15 +1,37 @@
 package com.example.backend.project;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProjectControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getAll_Returns401WhenNotLoggedIn() throws Exception {
+        mvc.perform(get("/api/projects"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void getAll_ReturnsEmptyListWhenLoggedIn_NoProjectsRegistered() throws Exception {
+        mvc.perform(get("/api/projects"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
 
 
 }
