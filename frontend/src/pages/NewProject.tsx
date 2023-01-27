bd47b1f09a7c4392ce41b19535b271a12be1895f
Christian Weck <weck.chr@gmail.com>
2023-01-27 16:21:20 +0100

    Refactored NewProject page with Bootstrap components

--- frontend/src/pages/NewProject.tsx
+++ frontend/src/pages/NewProject.tsx
@@ -1,5 +1,18 @@
+import "./NewProject.css"
 import Menu from "../components/Menu";
 import {useNavigate} from "react-router-dom";
+import {Form} from "react-bootstrap";
+
+type Project = {
+    id: string,
+    projectName: string,
+    createdAt: string,
+    plannedStartDate: string,
+    plannedFinishDate: string,
+    projectStatus: string,
+    assessorName: string,
+    projectDetails: string
+}
 
 export default function NewProject() {
 
@@ -10,11 +23,34 @@ export default function NewProject() {
             <Menu/>
             <h4>New Project Page</h4>
 
+            <Form>
+                <Form.Group>
+                    <Form.Label>Project Name</Form.Label>
+                    <Form.Control placeholder={"Project Name"}
+                                  name={"ProjectName"}></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Project Dates</Form.Label>
+                    <div>
+                        <Form.Label>Planned Start Date</Form.Label>
+                        <Form.Control placeholder={"Planned Start Date"}
+                                      type={"date"}></Form.Control>
+                    </div>
+                    <div>
+                        <Form.Label>Planned Finish Date</Form.Label>
+                        <Form.Control placeholder={"Planned Start Date"}
+                                      type={"date"}></Form.Control>
+                    </div>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Project Description</Form.Label>
+                    <Form.Control placeholder={"Please enter specific details on your project"}></Form.Control>
+                </Form.Group>
+
+
+            </Form>
+
             <div>
-                <input placeholder={"Project Name"}/>
-                <input placeholder={"Planned Start Date"}/>
-                <input placeholder={"Planned End Date"}/>
-                <input placeholder={"Project Description"}/>
                 <div>
                     <button onClick={() => navigate("/riskdetails")}>Add</button>
                 </div>
