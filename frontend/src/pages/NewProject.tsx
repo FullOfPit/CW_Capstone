373cf5ff4ced8da5527c06a356a0233d1f3e6b61
Christian Weck <weck.chr@gmail.com>
2023-01-29 10:14:02 +0100

    Implemented projectID lock logic

--- frontend/src/pages/NewProject.tsx
+++ frontend/src/pages/NewProject.tsx
@@ -2,7 +2,6 @@ import "./NewProject.css"
 import Menu from "../components/Menu";
 import {useNavigate} from "react-router-dom";
 import {Button, Form} from "react-bootstrap";
-import RiskSummaryCard from "../components/RiskSummaryCard";
 import React, {useState} from "react";
 
 type Project = {
@@ -35,6 +34,7 @@ export default function NewProject() {
 
     const [project, setProject] = useState<Project>(emptyProject);
     const navigate = useNavigate();
+    const [projectIdLock, setProjectIdLock] = useState<boolean>(false);
 
     const editProject = (event: React.ChangeEvent<HTMLInputElement>) => {
         const name = event.target.name;
@@ -45,6 +45,7 @@ export default function NewProject() {
         })
     };
 
+
     return (
         <div className={"ScreenLimit"}>
             <Menu/>
@@ -53,21 +54,31 @@ export default function NewProject() {
             <Form>
                 <Form.Group className={"NewProjectHead"}>
                     <Form.Label>Project Name:</Form.Label>
-                    <Form.Control placeholder={"Project Name"}
+                    <Form.Control placeholder={project.projectName || "Project Name"}
                                   name={"projectName"}
                                   value={project.projectName}
                                   onChange={editProject}
                     ></Form.Control>
                 </Form.Group>
-                <Form.Group className={"NewProjectHead"}>
-                    <Form.Label>Project ID:</Form.Label>
-                    <Form.Control placeholder={"Project ID"}
-                                  name={"projectId"}
-                                  value={project.projectId}
-                                  maxLength={26}
-                                  onChange={editProject}
-                    ></Form.Control>
-                </Form.Group>
+
+                {!projectIdLock ?
+                    <Form.Group className={"NewProjectHead"}>
+                        <Form.Label>Project ID:</Form.Label>
+                        <Form.Control placeholder={project.projectId || "Project ID"}
+                                      name={"projectId"}
+                                      value={project.projectId}
+                                      maxLength={26}
+                                      onChange={editProject}
+                        ></Form.Control>
+                        <Button onClick={() => setProjectIdLock(true)}>Submit</Button>
+                    </Form.Group>
+                : <div className={"NewProjectHead"}>
+                        <p>Project Id:</p>
+                        <p>{project.projectId}</p>
+                    </div>
+                }
+
+
                 <Form.Group className={"NewProjectDates"}>
                     <div>
                         <Form.Label>Planned Start Date</Form.Label>
@@ -99,8 +110,7 @@ export default function NewProject() {
                 </Form.Group>
 
                 <div className={"RiskDetailCards"}>
-                    <RiskSummaryCard projectId={"Test"} riskName={"Test"} healthHazard={1} probability={1} frequency={1}/>
-                    <Button onClick={() => navigate("/riskdetails")}>Add</Button>
+                    <Button onClick={() => navigate("/riskdetails")}>Assess New Risk Factor</Button>
                 </div>
 
                 <Form.Group className={"NewProjectHead"}>
@@ -114,7 +124,7 @@ export default function NewProject() {
 
                 <div>
                     <Button>Cancel</Button>
-                    <Button>Save</Button>
+                    <Button type={"submit"}>Save</Button>
                     <>{console.log(project)}</>
                 </div>
 
