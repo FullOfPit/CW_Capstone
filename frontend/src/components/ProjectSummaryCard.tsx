e06b4ecec6ccb51e1237dc069f546d4e7b04a18b
Christian Weck <weck.chr@gmail.com>
2023-01-31 10:55:09 +0100

    Minor Changes Risk Summary Card

--- frontend/src/components/ProjectSummaryCard.tsx
+++ frontend/src/components/ProjectSummaryCard.tsx
@@ -1,12 +1,20 @@
 import Project from "../types/Project";
+import "./ProjectSummaryCard.css"
+import {Button} from "react-bootstrap";
 
 export default function ProjectSummaryCard({project}:{project: Project}) {
 
     return (
-        <div>
-            <p>{project.projectName}</p>
-            <p>{project.projectStatus}</p>
-            <p>{project.assessorName}</p>
+        <div className={"ProjectSummaryCard"}>
+            <div>
+                <p>{project.projectName}</p>
+                <p>{project.projectStatus}</p>
+                <p>{project.assessorName}</p>
+            </div>
+            <div className={"ProjectSummaryCardButtons"}>
+                <Button>Edit Project</Button>
+                <Button>Delete Project</Button>
+            </div>
         </div>
     )
 
